772696e9276fcbd6551e576bdb329a091736edd7
Nimeshan <nimeshan995@gmail.com>
2025-08-31 12:19:07 +0530

    Initial Commit

---
+++ MyShoppingList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyShoppingList
+//
+//  Created by Nimeshan Shanmugam on 2025-08-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyShoppingList/MyShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShoppingListApp.swift
+//  MyShoppingList
+//
+//  Created by Nimeshan Shanmugam on 2025-08-31.
+//
+
+import SwiftUI
+
+@main
+struct MyShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
